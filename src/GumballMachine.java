0cc0baed79ae0dfc9f0affe06b3f58648e4baeeb
Thanmai Gajam <thanmai.gajam@sjsu.edu>
2020-10-25 18:11:04 -0700

    Gumball Machines are ready.

--- src/GumballMachine.java
+++ src/GumballMachine.java
@@ -1,42 +1,126 @@
-public class GumballMachine
-{
+public class GumballMachine {
 
-    private int num_gumballs;
-    private boolean has_quarter;
+	public enum Coin {
 
-    public GumballMachine( int size )
-    {
-        // initialise instance variables
-        this.num_gumballs = size;
-        this.has_quarter = false;
-    }
+		QUATER(25), NICKEL(5), DIME(10);
 
-    public void insertQuarter(int coin)
-    {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
-    }
-    
-    public void turnCrank()
-    {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
-    }
+		private final int value;
+
+		Coin(final int newValue) {
+			value = newValue;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+	}
+
+	protected int gumballCount;
+	protected int price;
+	protected boolean acceptsAnyCoin;
+	protected int amount;
+	protected boolean isTurnable;
+
+	public GumballMachine(int initialGumballCount, int price, boolean acceptsAnyCoin) {
+		this.gumballCount = initialGumballCount;
+		this.price = price;
+		this.acceptsAnyCoin = acceptsAnyCoin;
+
+	}
+
+	public void insertCoins(Coin coin) {
+
+		if (isTurnable) {
+			System.out.println("Turn the crank before inserting another coin");
+		} else {
+			this.isTurnable = true;
+			switch (coin) {
+			case QUATER: {
+				this.amount = amount + Coin.QUATER.getValue();
+				break;
+			}
+			case DIME: {
+				if (acceptsAnyCoin) {
+					this.amount = amount + Coin.DIME.getValue();
+				}
+				break;
+			}
+			case NICKEL: {
+				if (acceptsAnyCoin) {
+					this.amount = amount + Coin.NICKEL.getValue();
+				}
+				break;
+			}
+			}
+			this.isTurnable = true;
+		}
+
+	}
+
+//	abstract public void turnCrank();
+
+	public void turnCrank() {
+		if (!this.isTurnable) {
+			System.out.println("Insert coin to turn the crank.");
+		} else if(gumballCount == 0){
+			System.out.println("Machine out of gumballs.");
+		} else {
+			if (price <= amount) {
+				gumballCount--;
+				amount = 0;
+				System.out.println("Here is your gumball.");
+			} else {
+				System.out.println("Not enough money.");
+			}
+
+			this.isTurnable = false;
+		}
+	}
+
+	public static void main(String args[]) {
+		GumballMachine gm25 = new GumballMachine(1, 25, false);
+		gm25.insertCoins(Coin.QUATER);
+		gm25.turnCrank();
+		gm25.insertCoins(Coin.DIME);
+		gm25.turnCrank();
+		gm25.insertCoins(Coin.QUATER);
+		gm25.turnCrank();
+		gm25.turnCrank();
+
+		System.out.println("--------");
+
+		GumballMachine gm50 = new GumballMachine(10, 50, false);
+		gm50.insertCoins(Coin.QUATER);
+		gm50.turnCrank();
+		gm50.insertCoins(Coin.DIME);
+		gm50.insertCoins(Coin.DIME);
+		gm50.insertCoins(Coin.NICKEL);
+		gm50.turnCrank();
+		gm50.insertCoins(Coin.QUATER);
+		gm50.turnCrank();
+		gm50.insertCoins(Coin.QUATER);
+		gm50.turnCrank();
+		gm50.insertCoins(Coin.QUATER);
+		gm50.turnCrank();
+
+		System.out.println("--------");
+
+		GumballMachine gm50any = new GumballMachine(10, 50, true);
+		gm50any.insertCoins(Coin.QUATER);
+		gm50any.turnCrank();
+		gm50any.insertCoins(Coin.DIME);
+		gm50any.insertCoins(Coin.DIME);
+		gm50any.insertCoins(Coin.DIME);
+		gm50any.turnCrank();
+		gm50any.insertCoins(Coin.DIME);
+		gm50any.insertCoins(Coin.DIME);
+		gm50any.insertCoins(Coin.NICKEL);
+		gm50any.turnCrank();
+		gm50any.insertCoins(Coin.DIME);
+		gm50any.insertCoins(Coin.DIME);
+		gm50any.insertCoins(Coin.NICKEL);
+		gm50any.insertCoins(Coin.QUATER);
+		gm50any.turnCrank();
+	}
 }
